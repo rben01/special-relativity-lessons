05f4057fb1033d744218f96f33084ce4d6d22744
Robert Bennett <rltbennett@icloud.com>
2025-04-22 13:12:42 -0400

    Still trying to get paths to work both locally and on gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+const astroPath = "_astro";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://rben01.github.io",
-	// the github repo
-	base: "special-relativity-lessons",
-	// second path component must match base
+	// the github repo, twice
+	base: `special-relativity-lessons/special-relativity-lessons`,
+	// second path component must also be the github repo
 	outDir: "_output/special-relativity-lessons",
 });
