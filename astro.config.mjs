f98f6c37a9910f03b071aeb4bb00710f673f6983
Robert Bennett <rltbennett@icloud.com>
2025-04-22 11:47:14 -0400

    Publishing to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	base: "_astro",
-	outDir: "_output/_astro",
+	site: "https://rben01.github.io",
+	// the github repo
+	base: "special-relativity-lessons",
+	// second path component must match base
+	outDir: "_output/special-relativity-lessons",
 });
