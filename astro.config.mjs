cd78c8a62d931711e59ca1e6116820c3da875bc0
Robert Bennett <rltbennett@icloud.com>
2025-05-08 23:19:45 -0400

    Fixed astro config to work both locally and with GH actions (?)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-const astroPath = "_astro";
+const runningOnGithub = process.env.GITHUB_ACTIONS === "true";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://rben01.github.io",
 	// the github repo, twice
-	base: `special-relativity-lessons/special-relativity-lessons`,
+	base: runningOnGithub
+		? "special-relativity-lessons/special-relativity-lessons"
+		: "special-relativity-lessons",
 	// second path component must also be the github repo
 	outDir: "_output/special-relativity-lessons",
 });
